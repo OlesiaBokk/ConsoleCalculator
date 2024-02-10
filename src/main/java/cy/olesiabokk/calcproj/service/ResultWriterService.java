14cde12aeee1371c74793295091957291d22e380
OlesiaBokk <orbokk@gmail.com>
2024-02-10 20:38:26 +0200

    just little improved readability

--- src/main/java/cy/olesiabokk/calcproj/service/ResultWriterService.java
+++ src/main/java/cy/olesiabokk/calcproj/service/ResultWriterService.java
@@ -4,6 +4,7 @@ import cy.olesiabokk.calcproj.entity.Entities;
 
 public class ResultWriterService {
     public static void printResult(double num1, double num2, double result, char operator, String operatorName) {
-        System.out.println(operatorName + "\n" + Entities.converter(num1) + " " + operator + " " + Entities.converter(num2) + " = " + Entities.converter(result));
+        System.out.println(operatorName + "\n" + Entities.converter(num1) + " " + operator + " " +
+                Entities.converter(num2) + " = " + Entities.converter(result));
     }
 }
