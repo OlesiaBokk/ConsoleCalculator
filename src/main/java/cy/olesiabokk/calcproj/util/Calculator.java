2039af3c87edb9a8931f38b7fd7c79f563dd01d4
OlesiaBokk <orbokk@gmail.com>
2024-02-11 13:07:16 +0200

    Refactor methods in Calculator class

--- src/main/java/cy/olesiabokk/calcproj/util/Calculator.java
+++ src/main/java/cy/olesiabokk/calcproj/util/Calculator.java
@@ -1,25 +1,20 @@
 package cy.olesiabokk.calcproj.util;
 
 public class Calculator {
+    public static double value;
     public static double add(double num1, double num2) {
-        double value = num1 + num2;
-        return value;
+        return value = num1 + num2;
     }
 
     public static double subtract(double num1, double num2) {
-        double value = num1 - num2;
-        return value;
+        return value = num1 - num2;
     }
 
     public static double multiply(double num1, double num2) {
-        double value = num1 * num2;
-        return value;
+        return value = num1 * num2;
     }
 
     public static double divide(double num1, double num2) {
-        double first = num1;
-        double second = num2;
-        double value = first / second;
-        return value;
+        return value = num1 / num2;
     }
 }
